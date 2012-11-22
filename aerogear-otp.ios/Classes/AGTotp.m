0205781715a8bea81f25e8189eaff97e73bffc53
Matthias Wessendorf <matzew@apache.org>
2012-11-22 10:30:58 +0100

    Renaming classes to have AeroGear specific prefix and moving them into the Classes folder

---
+++ aerogear-otp.ios/Classes/AGTotp.m
@@ -0,0 +1,50 @@
+//
+//  Taken with small modifications
+//
+//  Copyright 2011 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License.  You may obtain a copy
+//  of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+//  License for the specific language governing permissions and limitations under
+//  the License.
+//
+
+#import "AGTotp.h"
+
+const NSUInteger defaultInterval = 30;
+
+@interface AGTotp ()
+
+@end
+
+@implementation AGTotp
+
+- (id)initWithSecret:(NSData *)secret {
+  if ((self = [super initWithSecret:secret])) {
+  }
+  return self;
+}
+
+- (NSString *)generateOTP {
+  return [self now:[NSDate date]];
+}
+
+- (NSString *)now:(NSDate *)date {
+  if (!date) {
+    // If no now date specified, use the current date.
+    date = [NSDate date];
+  }
+
+  NSTimeInterval seconds = [date timeIntervalSince1970];
+  uint64_t counter = (uint64_t)(seconds / defaultInterval);
+  return [super generateOTPForCounter:counter];
+}
+
+@end
